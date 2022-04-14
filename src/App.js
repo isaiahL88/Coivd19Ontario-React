b865a5bc85726220a5d6d7c450412126b8a71cff
isaiahL88 <isaiah.ib@outlook.com>
2022-04-13 23:16:47 -0400

    Initial commit, basic components created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import Home from './Home.js';
+import Nav from './Nav.js'
+import styles from './App.css'
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Nav></Nav>
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
